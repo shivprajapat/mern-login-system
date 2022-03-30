8249a91bbe8932fb4f7cf6d0806510e102fdf6d6
shivprajapat <shivkumarprajapat232@gmail.com>
2022-03-30 17:33:49 +0530

    Register User & Hash Password functionally Added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,7 +16,34 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please add all fields");
   }
 
-  res.json({message:"Register user"})
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 /**

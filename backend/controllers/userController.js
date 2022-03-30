746525a625d93ec7983a41da38e3811c8e3367fe
shivprajapat <shivkumarprajapat232@gmail.com>
2022-03-30 17:59:33 +0530

    Authenticate User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,21 @@ const registerUser = asyncHandler(async (req, res) => {
  **/
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login User" });
+  const { password, email } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 /**
